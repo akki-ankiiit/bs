d8216df5edecc0269184dd60c26214b151a0a3bf
Akki <akki@Akkis-MacBook-Air.local>
2026-06-26 04:05:12 +0530

    Bring Why Us section down on mobile

--- src/pages/Homie.jsx
+++ src/pages/Homie.jsx
@@ -495,7 +495,7 @@ export default function Homie({ themeKey = 'classic', typeKey = 'acidGaraHelv' }
         <PosterCarousel items={carouselPosters} />
 
         {/* WHY US */}
-        <div style={{ position: 'relative', zIndex: 2, height: isMobile ? 'auto' : 500, paddingBottom: isMobile ? 40 : 0, margin: '0 auto', marginTop: isMobile ? 0 : -40, width: '100%', maxWidth: 1200 }}>
+        <div style={{ position: 'relative', zIndex: 2, height: isMobile ? 'auto' : 500, paddingBottom: isMobile ? 40 : 0, margin: '0 auto', marginTop: isMobile ? 40 : -40, width: '100%', maxWidth: 1200 }}>
 
           <style>{`
             @keyframes orbitFloat {
